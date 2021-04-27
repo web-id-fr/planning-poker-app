ef4ca4bc579f13cea533ec47848ca2b05a7b8722
mansmahamat <mansour.mahamat.salle@gmail.com>
2021-04-27 17:50:57 +0200

    :art: design card
    design card

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { Route, NativeRouter, Link } from "react-router-native";
 import {Number} from "./pages/Number";
 import {Emoji} from "./pages/Emoji";
@@ -7,22 +7,25 @@ import {Emoji} from "./pages/Emoji";
 
 export default function App() {
   return (
-    <NativeRouter>
+    <ScrollView>
+      <NativeRouter>
       <View style={styles.container}>
           <Number />
         <Link to="/emoji">
           <Text>Emoji page</Text>
         </Link>
-        <Route exact path="/" component={Number} />
         <Route path="/emoji" component={Emoji} />
       </View>
     </NativeRouter>
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 80,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
--- pages/Number.js
+++ pages/Number.js
@@ -1,14 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NativeRouter, Route, Link } from "react-router-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const Number = () => {
-    return ( 
-        <View>
-            <Text>
-                Number
-            </Text>
-        </View>
-     );
-}
- 
+  const [selectedText, setSelectedText] = useState();
+
+  const handleClick = (number) => {
+    setSelectedText(number)
+  };
+
+  return (
+    <>
+      <View style={style.container}>
+        {numbers.map((number, index) => (
+          <TouchableOpacity
+            onPress={() => {
+              handleClick(number);
+            }}
+            key={index}
+            style={style.cardStyle}
+          >
+            <Text style={style.textStyle}>{number}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Text style={style.textSelected}>{selectedText}</Text>
+    </>
+  );
+};
+
+var style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+  cardStyle: {
+    flexBasis: "40%",
+    marginTop: 10,
+    marginRight: 10,
+    height: 110,
+    justifyContent: "center",
+    backgroundColor: "lightpink"
+  },
+  textStyle: {
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+  textSelected: {
+   marginTop: 50
+  },
+});
