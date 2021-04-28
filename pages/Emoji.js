f1c28c9bda7c772348464952ab0265694a9fa378
Valentin <valentin@web-id.fr>
2021-04-28 09:34:40 +0200

    ✨ Add emoji selection

--- pages/Emoji.js
+++ pages/Emoji.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React,  { useState } from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
 import { NativeRouter, Route, Link } from "react-router-native";
 
+import EmojiSelector from "react-native-emoji-selector";
+
 export const Emoji = () => {
-    return ( 
-        <View>
-            <Text>
-                Emoji
-            </Text>
+  const [showEmojiKeyboard, setShowEmojiKeyboard] = useState(false)
+  const [selectedEmoji, setSelectedEmoji] = useState('😇')
+  const handleEmojiKeyboard = () => setShowEmojiKeyboard(!showEmojiKeyboard);
+  const handleEmojiChange = emoji => {handleEmojiKeyboard(); setSelectedEmoji(emoji)};
+
+  return (
+    <View>
+      <View style={styles.mainContainer}>
+        <Button
+          onPress={handleEmojiKeyboard}
+          title={selectedEmoji}
+          accessibilityLabel="Show emoji keyboard "
+        />
+      </View>
+      {showEmojiKeyboard && (
+        <View style={styles.emojiContainer}>
+          <EmojiSelector
+            columns="9"
+            onEmojiSelected={(emoji) => handleEmojiChange(emoji)}
+          />
         </View>
-     );
-}
- 
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: "30%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emojiContainer: {
+    height: "40%",
+  },
+});
