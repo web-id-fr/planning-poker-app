e1509e682999cba1ccc0173473b81e5ec626f5f9
francoisWebID <37926647+francoisWebID@users.noreply.github.com>
2021-04-29 10:07:40 +0200

    Ch2061/créer un dossier backend et déposer un nouveau (#3)
    * :sparkles: init laravel
    
    * :art: Added ignore for .idea folder
    
    Co-authored-by: François Bouladou <bouladou.francois@gmail.com>

---
+++ backend/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * @var array<string, string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
