a42ce15218b735c54b3fa68508f0216c9a2aff5b
Valentin <valentin@web-id.fr>
2021-04-30 06:47:13 +0200

    🎨 remove safeareaview

--- App/pages/Number.js
+++ App/pages/Number.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+// import { SafeAreaView } from "react-native";
 import { StyleSheet, View } from "react-native";
 
 import { EstimationCard } from "../components/estimation/EstimationCard";
@@ -20,16 +20,13 @@ export const Number = ({appStep, setAppStep}) => {
   const items = [0, "1/2", 1, 2, 3, 5, 8, 13, 20, 40, 100, "?"];
 
   return (
-    <SafeAreaView>
-      <View style={[selectedCard !== null ? style["container--selected"] : style.container]}>
-        {items.map((item, index) => (
-          appStep === "estimation-selection" || index === selectedCard ?
-            <EstimationCard key={item} index={index} cardTitle={item} handleCardClick={handleCardClick} appStep={appStep} /> :
-            null
-        ))}
-      </View>
-    </SafeAreaView>
-      
+    <View style={[selectedCard !== null ? style["container--selected"] : style.container]}>
+      {items.map((item, index) => (
+        appStep === "estimation-selection" || index === selectedCard ?
+          <EstimationCard key={item} index={index} cardTitle={item} handleCardClick={handleCardClick} appStep={appStep} /> :
+          null
+      ))}
+    </View>     
   );
 };
 
